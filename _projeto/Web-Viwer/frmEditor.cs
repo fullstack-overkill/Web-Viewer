406d7f37370ebe3db13f6b61fd0c63aadee68743
christopher mms <christophermms.ctt@gmail.com>
2019-03-11 18:31:05 -0300

    Inrterface principal
    Interface do frmEditor Pronta ! agora faltão ous outros.

---
+++ _projeto/Web-Viwer/frmEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Viwer
+{
+    public partial class frmEditor : Form
+    {
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
